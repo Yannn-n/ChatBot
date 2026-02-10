98a6064b72380709294c40b05790c4eeeef3188c
Yannn-n <ryandarifki1@gmail.com>
2026-02-10 20:37:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,26 +8,33 @@ async function kirim() {
   chat.innerHTML += `<p class="user"><b>Kamu:</b> ${pesan}</p>`;
   input.value = "";
 
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/gpt2",
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer 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",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        inputs: pesan
-      })
+  try {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/gpt2",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer hf_EEtdjUfhY0kKNotfzrEiNb0sAYOtWNHKP",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ inputs: pesan })
+      }
+    );
+
+    const data = await response.json();
+
+    if (Array.isArray(data)) {
+      chat.innerHTML += `<p class="bot"><b>Bot:</b> ${data[0].generated_text}</p>`;
+    } else if (data.generated_text) {
+      chat.innerHTML += `<p class="bot"><b>Bot:</b> ${data.generated_text}</p>`;
+    } else if (data.error) {
+      chat.innerHTML += `<p class="bot"><b>Error:</b> ${data.error}</p>`;
+    } else {
+      chat.innerHTML += `<p class="bot"><b>Error:</b> Respon tidak dikenali</p>`;
     }
-  );
 
-  const data = await response.json();
-
-  if (data.error) {
-    chat.innerHTML += `<p class="bot"><b>Bot:</b> ${data.error}</p>`;
-  } else {
-    chat.innerHTML += `<p class="bot"><b>Bot:</b> ${data[0].generated_text}</p>`;
+  } catch (err) {
+    chat.innerHTML += `<p class="bot"><b>Error:</b> ${err.message}</p>`;
   }
 
   chat.scrollTop = chat.scrollHeight;
